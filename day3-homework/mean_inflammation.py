aec4f6beeac941519034a54528e244b784de2e0f
Sarah <shanso20@jh.edu>
2023-09-07 17:23:09 -0400

    Exercise 3 Answers

---
+++ day3-homework/mean_inflammation.py
@@ -0,0 +1,30 @@
+#!/usr/bilbn/env python
+import sys
+
+fname = sys.argv[1]
+data = open(fname).readlines()
+
+#print(data) #to check that file was imported
+
+def my_mean(list_name):
+	sum_list = sum(list_name)
+	average = sum_list/len(list_name)
+	return average
+
+integers = []
+for x in data:
+	x = x.rstrip("\n")
+	x = x.split(",")
+
+# print(x) will give a list of lists, each containing the values as strings and with the extra return removed
+	numbers = []
+	for y in x:
+		y = int(y)
+		numbers.append(y)
+	integers.append(numbers)
+
+# print(integers) to check that you now have a list of lists containing integers
+# need to select one patient whose inflammation values I will average.
+
+# print(integers[4]) to check that I can select just the values for patient 5
+print(my_mean(integers[4]))
