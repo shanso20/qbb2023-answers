65137393f93a5814a261a8a196454708c32aa19d
Sarah <shanso20@jh.edu>
2023-09-07 17:35:41 -0400

    Fixed answer to exercise 3

--- day3-homework/mean_inflammation.py
+++ day3-homework/mean_inflammation.py
@@ -11,20 +11,25 @@ def my_mean(list_name):
 	average = sum_list/len(list_name)
 	return average
 
-integers = []
-for x in data:
-	x = x.rstrip("\n")
-	x = x.split(",")
+def patient_average(patient_index):
+
+	integers = []
+	for x in data:
+		x = x.rstrip("\n")
+		x = x.split(",")
 
 # print(x) will give a list of lists, each containing the values as strings and with the extra return removed
-	numbers = []
-	for y in x:
-		y = int(y)
-		numbers.append(y)
-	integers.append(numbers)
+		numbers = []
+		for y in x:
+			y = int(y)
+			numbers.append(y)
+		integers.append(numbers)
 
 # print(integers) to check that you now have a list of lists containing integers
 # need to select one patient whose inflammation values I will average.
 
 # print(integers[4]) to check that I can select just the values for patient 5
-print(my_mean(integers[4]))
+	answer = my_mean(integers[patient_index])
+	return answer
+
+print(patient_average(4))
